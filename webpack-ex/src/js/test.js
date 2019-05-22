10deb2d32fbc39391643a24f8a550cf8b1cdb4d3
Lee HyunSu <dlgustn555@naver.com>
2019-05-23 02:47:51 +0900

    css 로더  추가 https://github.com/dlgustn555/prototypes/issues/1

--- webpack-ex/src/js/test.js
+++ webpack-ex/src/js/test.js
@@ -1,6 +1,7 @@
 import Mustache from 'mustache'
 import { blackText } from '../template'
 import '../style/text.css'
+import '../style/greenText.css'
 
 export default () => {
   document.body.innerHTML = Mustache.render(blackText)
