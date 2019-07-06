ceae033a77a7b87a588ff69db09edf7aebb8c054
dlgustn555 <dlgustn555@naver.com>
2019-07-06 20:36:58 +0900

    index.ts 파일 제거

--- sample/server/src/index.ts
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const PORT = 9003;
-
-const server = express();
-
-server.listen(PORT, () => {
-    console.log(`Sample Server is Running in PORT : ${PORT}`);
-})
