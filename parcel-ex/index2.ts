b82f90299ddbfde3fa9283c2f672c92934bdd00e
Lee HyunSu <dlgustn555@naver.com>
2019-07-24 03:28:11 +0900

    [예제] HTML5의 template 태그 사용 https://github.com/dlgustn555/prototypes/issues/7

--- parcel-ex/index2.ts
+++ parcel-ex/index2.ts
@@ -1,12 +1,8 @@
+import './src/style/index2.css';
 import { getSelector } from "./src/util";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const popupTemplate = getSelector('#popup_template');
-    console.log('content' in popupTemplate);
-
-    const oTest = {
-        content: { text: 'abcdefg' }
-    };
-
-    console.log('content' in oTest);
+    const popupTemplate: any = getSelector('#popup_template');
+    const clone = document.importNode(popupTemplate.content, true);
+    document.body.appendChild(clone);
 });
